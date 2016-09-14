44365173d103f91638a2971ec0fca07ba00c7261
Sam Corcos <sam@corcos.io>
2016-09-14 05:14:21 -0400

    routes and views

---
+++ app/components/Settings/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export class Settings extends React.Component {
+  render() {
+    return (
+      <div>Settings component</div>
+    )
+  }
+}
+
+export default Settings
