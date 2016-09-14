64a730eb026bb3a28e9f4ef36333317e78ab1294
Sam Corcos <sam@corcos.io>
2016-09-14 06:00:07 -0400

    login and signup forms

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -3,12 +3,26 @@ import cssModules from "react-css-modules"
 import style from "./style.css"
 
 import { default as Signup } from "../Signup"
+import { default as Login } from "../Login"
 
 export class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formState: "signup"
+    }
+  }
+
   render() {
     return (
-      <div>
-        <Signup />
+      <div className={style.leader}>
+        <h1 className={style.title}>Phoenix Chat</h1>
+        {this.state.formState === "signup" ? <Signup /> : null}
+        {this.state.formState === "login" ? <Login /> : null}
+        <img
+          role="presentation"
+          className={style.circles}
+          src="https://s3.amazonaws.com/learnphoenix-static-assets/images/circles-full.png" />
       </div>
     )
   }
