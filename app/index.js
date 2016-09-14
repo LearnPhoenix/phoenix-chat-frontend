be3c5699ce6b715946741e51a71aef712a99f94a
Sam Corcos <sam@corcos.io>
2016-09-14 05:10:54 -0400

    basics of webpack

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+class HelloWorld extends React.Component {
+  render() {
+    return (<div>Hello World!</div>)
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './app/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'app')
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '', '.js' ]
+  }
+}
