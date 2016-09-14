2950d906922f91248b1f9580425c298356c3526a
Sam Corcos <sam@corcos.io>
2016-09-14 06:52:19 -0400

    move logic to redux

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux"
+import { createStore, compose, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
 import reducers from "./reducers"
 
-const store = createStore(reducers)
+const middlewares = [thunk]
+
+const createStoreWrapper = compose(
+  applyMiddleware(...middlewares)
+)(createStore)
+
+const store = createStoreWrapper(reducers)
 
 export default store
