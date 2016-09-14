fe214c1fdb42066cb838659790e9accf304f222f
Sam Corcos <sam@corcos.io>
2016-09-14 05:11:10 -0400

    basics of webpack

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 import React from "react"
+import ReactDOM from "react-dom"
 
 class HelloWorld extends React.Component {
   render() {
     return (<div>Hello World!</div>)
   }
 }
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById("root")
+)
