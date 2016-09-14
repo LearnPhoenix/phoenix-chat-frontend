0aeb33ca4d38acfbe3c95a4ef029300a39c801d3
Sam Corcos <sam@corcos.io>
2016-09-14 06:52:46 -0400

    move logic to redux

--- app/redux/actions.js
+++ app/redux/actions.js
@@ -22,12 +22,32 @@ Actions.userNew = function userNew(user) {
 }
 
 Actions.userLogin = function userLogin(user) {
-  return {
-    type: "USER_LOGIN",
-    payload: {
-      user
-    }
-  }
+  return dispatch => fetch("http://localhost:4000/auth/identity/callback", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: user.email,
+      password: user.password
+    })
+  })
+  .then((res) => { return res.json() })
+  .then((res) => {
+    /* If success, log the user in */
+    localStorage.token = res.data.token
+    /* Then send action to reducer */
+    dispatch({
+      type: "USER_LOGIN",
+      payload: {
+        user: res.data
+      }
+    })
+  })
+  .catch((err) => {
+    console.warn(err)
+  })
 }
 
 export default Actions
