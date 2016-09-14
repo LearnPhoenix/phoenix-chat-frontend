e531dcca324d51b2eb7273334c6e20b4ff0e7ecc
Sam Corcos <sam@corcos.io>
2016-09-14 06:52:57 -0400

    move logic to redux

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,12 +1,22 @@
 import { combineReducers } from "redux"
 
-function user(state = {}, action) {
+function user(state = {
+  email: "",
+  username: "",
+  id: ""
+}, action) {
   switch (action.type) {
     case "USER_NEW":
-      return state
+      return Object.assign({}, state, {
+        email: action.payload.user.email,
+        username: action.payload.user.username,
+        id: action.payload.user.id
+      })
     case "USER_LOGIN":
       return Object.assign({}, state, {
-        email: action.payload.user.email
+        email: action.payload.user.email,
+        username: action.payload.user.username,
+        id: action.payload.user.id
       })
     default: return state
   }
