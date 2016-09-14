54a60520999ce837419b60e20a0a3011d7809bf1
Sam Corcos <sam@corcos.io>
2016-09-14 06:52:33 -0400

    move logic to redux

--- app/redux/actions.js
+++ app/redux/actions.js
@@ -1,12 +1,24 @@
 const Actions = {}
 
 Actions.userNew = function userNew(user) {
-  return {
-    type: "USER_NEW",
-    payload: {
-      user
-    }
-  }
+  return dispatch => fetch("http://localhost:4000/api/users", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ user })
+  })
+  .then((res) => {
+    return res.json()
+  })
+  .then((res) => {
+    // TODO: More on this later
+    console.log(res)
+  })
+  .catch((err) => {
+    console.warn(err)
+  })
 }
 
 Actions.userLogin = function userLogin(user) {
