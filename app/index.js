c853a2498fdf3fbf0baf129c19a2f047ee8f42a2
Sam Corcos <sam@corcos.io>
2016-09-14 05:14:50 -0400

    routes and views

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Router, Route, IndexRoute, hashHistory } from "react-router"
 
-class HelloWorld extends React.Component {
-  render() {
-    return (<div>Hello World!</div>)
-  }
-}
+import { default as Home } from "./components/Home"
+import { default as Settings } from "./components/Settings"
+
+const App = props => (<div>{props.children}</div>)
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="settings" component={Settings} />
+    </Route>
+  </Router>,
   document.getElementById("root")
 )
