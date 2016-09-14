1c055d6bb906563d2ee194f058d79e8f94bd4560
Sam Corcos <sam@corcos.io>
2016-09-14 05:21:41 -0400

    set up styles in webpack

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Router, Route, IndexRoute, hashHistory } from "react-router"
 
+import "./styles/reset.css"
+
 import { default as Home } from "./components/Home"
 import { default as Settings } from "./components/Settings"
 
