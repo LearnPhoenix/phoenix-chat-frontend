aa07b326ea642bf504f319d704254c10cf729aa5
Sam Corcos <sam@corcos.io>
2016-09-14 06:08:02 -0400

    actions reducers store

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Router, Route, IndexRoute, hashHistory } from "react-router"
+import { Provider } from "react-redux"
+import store from "./redux/store"
 
 import "./styles/reset.css"
 
@@ -10,11 +12,13 @@ import { default as Settings } from "./components/Settings"
 const App = props => (<div>{props.children}</div>)
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="settings" component={Settings} />
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="settings" component={Settings} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 )
