a428d52b07bb0f1641926a3390ee59bcc0db0d78
Gabriel Jaldon <gjaldon85@gmail.com>
2016-09-16 16:55:45 +0800

    Connecting the Frontend to AdminChannel
    We listen to the presence_state and presence_diff events
    so we can keep track of and sync presences in the frontend.
    We then store these presences in our Chat component's
    state. That way, our component will rerender when
    presences' state changes. We pass these presences to
    Sidebar, which is a stateless functional component, and
    only deals with rendering of the presences.

--- app/components/Chat/index.js
+++ app/components/Chat/index.js
@@ -1,21 +1,67 @@
 import React from "react"
 import cssModules from "react-css-modules"
+import { Socket, Presence } from "phoenix"
+import { connect } from "react-redux"
 import style from "./style.css"
 
 import { default as Sidebar } from "../Sidebar"
 
 export class Chat extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      presences: {}
+    }
+  }
+
+  componentDidMount() {
+    const params = this.props.user
+    this.socket = new Socket("ws://localhost:4000/socket", { params })
+    this.socket.connect()
+    this.configureAdminChannel()
+  }
+
+  configureAdminChannel() {
+    this.adminChannel = this.socket.channel("admin:active_users")
+
+    this.adminChannel.on("presence_state", state => {
+      const presences = Presence.syncState(this.state.presences, state)
+      // added clearer logging to track presences
+      console.log('Presences after sync: ', presences)
+      this.setState({ presences })
+    })
+
+    this.adminChannel.on("presence_diff", state => {
+      const presences = Presence.syncDiff(this.state.presences, state)
+      // added clearer logging to track presences
+      console.log('Presences after diff: ', presences)
+      this.setState({ presences })
+    })
+
+    this.adminChannel.join()
+      .receive("ok", ({ id }) => {
+        // added clearer logging to track when a user joins a topic
+        console.log(`${id} succesfully joined the active_users topic.`)
+      })
+  }
+
   render() {
     return (
       <div>
-        <Sidebar />
+        <Sidebar
+          presences={this.state.presences} />
         <div className={style.chatWrapper}>
           chat me
         </div>
-        {this.props.children}
+        { this.props.children }
       </div>
     )
   }
 }
 
-export default cssModules(Chat, style)
+
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(cssModules(Chat, style))
--- app/components/Sidebar/index.js
+++ app/components/Sidebar/index.js
@@ -1,12 +1,33 @@
 import React from "react"
 import cssModules from "react-css-modules"
+import { Presence } from "phoenix"
 import style from "./style.css"
 
-export const Sidebar = () => {
+// Moved listBy and renderList functions to outside of the Sidebar function
+// since we don't these functions defined every time we run the Sidebar
+// function.
+const listBy = (id, { metas: [first, ...rest] }) => {
+  first.count = rest.length + 1
+  first.id = id
+  return first
+}
+
+const renderList = (props) => {
+  // No more filter code since we no longer include admins in the presences
+  return Presence.list(props.presences, listBy)
+    .map(({ id }) => {
+      return (
+        <div key={id}>
+          { id }
+        </div>
+      )
+    })
+}
+
+export const Sidebar = props => {
   return (
     <div className={style.sidebar}>
-      <h3>John Smith</h3>
-      <p>Last active: {Math.floor((Math.random() * 10) + 1)} minutes ago.</p>
+      { renderList(props) }
     </div>
   )
 }
