90515b6be142f359a4cce6cff2324e4b816e1bc3
Sam Corcos <sam@corcos.io>
2016-09-20 04:55:11 -0400

    connect the frontend to adminchannel

--- app/components/Chat/index.js
+++ app/components/Chat/index.js
@@ -1,10 +1,26 @@
 import React from "react"
 import cssModules from "react-css-modules"
+import { Socket, Presence } from "phoenix"
+import { connect } from "react-redux"
 import style from "./style.css"
 
 import { default as Sidebar } from "../Sidebar"
 
 export class Chat extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      presences: {}
+    }
+  }
+
+  componentDidMount() {
+    const params = this.props.user
+    this.socket = new Socket("ws://localhost:4000/socket", { params })
+    this.socket.connect()
+    this.configureAdminChannel()
+  }
+
   render() {
     return (
       <div>
@@ -18,4 +34,8 @@ export class Chat extends React.Component {
   }
 }
 
-export default cssModules(Chat, style)
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(cssModules(Chat, style))
